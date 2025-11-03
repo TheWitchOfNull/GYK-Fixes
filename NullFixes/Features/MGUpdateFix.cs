81bc21210e36fef9ef6d42164e7168e96588471a
TheWitchOfNull <47769190+TheWitchOfNull@users.noreply.github.com>
2025-11-02 20:38:45 -0900

    some fixes

---
+++ NullFixes/Features/MGUpdateFix.cs
@@ -0,0 +1,40 @@
+﻿using CodeStage.AdvancedFPSCounter;
+using HarmonyLib;
+using UnityEngine;
+using static MainGame;
+
+namespace NullFixes.Features;
+
+public static class MGUpdateFix
+{
+    public static void Install()
+    {
+        Log.Info("Installing feature: main game update fix");
+        var h = Plugin.HarmonyInstance;
+        var og = AccessTools.Method(typeof(MainGame), nameof(MainGame.Update));
+        var pf = new HarmonyMethod(typeof(MGUpdateFix).GetMethod(nameof(MainGame_Update_Prefix)));
+        h.Patch(og, prefix: pf);
+    }
+
+    public static bool MainGame_Update_Prefix()
+    {
+        //removes dead code, doesn't reset vsync every frame D:
+        if (MainGame.game_started)
+        {
+            MainGame.me.save.game_logics.Update();
+            if (!paused)
+            {
+                BuffsLogics.RecalculateBuffs();
+            }
+            if (MainGame.me.game_mode == GameMode.Building)
+            {
+                MainGame.me.build_mode_logics.Update();
+            }
+        }
+        if (GamePadController.cheat_combination_pressed)
+        {
+            Object.FindObjectOfType<AFPSCounter>().SwitchCounter();
+        }
+        return false;
+    }
+}
---
+++ NullFixes/Features/ThrottleLights.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NullFixes.Features;
+
+public static class ThrottleLights
+{
+    private const float TARGET_HZ = 20f;
+    private static float _lastUpdate = 0f;
+    public static void Install()
+    {
+        Log.Info("Installing feature: throttle lights");
+        var h = Plugin.HarmonyInstance;
+        var og = AccessTools.Method(typeof(DynamicLights), nameof(DynamicLights.Update));
+        var pf = new HarmonyMethod(typeof(ThrottleLights).GetMethod(nameof(DynamicLights_Update_Prefix)));
+        h.Patch(og, prefix: pf);
+    }
+
+    public static bool DynamicLights_Update_Prefix()
+    {
+        if (Time.realtimeSinceStartup - _lastUpdate < (1f / TARGET_HZ)) return false;
+        _lastUpdate = Time.realtimeSinceStartup;
+        return true;
+    }
+}
