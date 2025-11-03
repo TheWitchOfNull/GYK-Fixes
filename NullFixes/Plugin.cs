0fc1bb91e009ec6f0fc19417df45143e820762d5
TheWitchOfNull <47769190+TheWitchOfNull@users.noreply.github.com>
2025-11-02 21:54:43 -0900

    plugin code

---
+++ NullFixes/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using NullFixes.Features;
+
+namespace NullFixes;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static HarmonyLib.Harmony HarmonyInstance;
+    public void Awake()
+    {
+        Log.Initialize(Logger);
+        Log.Info($"NullFixes here. Put me in, coach.");
+        HarmonyInstance = new("NullFixes");
+
+        InstallFeatures();
+    }
+
+    private void InstallFeatures()
+    {
+        ThrottleLights.Install();
+        MGUpdateFix.Install();
+    }
+}
